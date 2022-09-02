32d3a9a51b3fe414c23e10baea729ca2ac0d3fcb
matipoklo <poklojr@gmail.com>
2022-09-02 16:08:08 +0200

    zrobiony program pokaz/ukryj zdjecie

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,32 @@
+
+
 package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //ffjfjfj
-        //jjjjjj
+        val imageView = findViewById<ImageView>(R.id.imageView2);
+        val button = findViewById<Button>(R.id.button);
+        var widocznosc = 0;
+        button.setOnClickListener(){
+            if (widocznosc == 1){
+                 imageView.setImageResource(R.drawable.ic_launcher_background)
+                widocznosc = 0;
+            }
+            else{
+                imageView.setImageResource(R.drawable.ic_launcher_foreground);
+                widocznosc = 1;
+            }
+        }
+
     }
 }
